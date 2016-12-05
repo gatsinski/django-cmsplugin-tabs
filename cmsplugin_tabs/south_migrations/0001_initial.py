beb0a98d7b24da86c9bd88a8e7bde44e2be64c4d
Hristo Gatsinski <h.gatsinski@magicsolutions.bg>
2016-12-05 15:41:26 +0200

    Squash Migration

--- cmsplugin_tabs/south_migrations/0001_initial.py
+++ cmsplugin_tabs/south_migrations/0001_initial.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import datetime
+from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
@@ -9,35 +9,37 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'CMSTabsList'
-        db.create_table('cmsplugin_cmstabslist', (
-            ('cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
+        db.create_table(u'cmsplugin_tabs_cmstabslist', (
+            (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
+            ('template', self.gf('django.db.models.fields.CharField')(default='cmsplugin_tabs/tabs.html', max_length=255)),
         ))
-        db.send_create_signal('cmsplugin_tabs', ['CMSTabsList'])
+        db.send_create_signal(u'cmsplugin_tabs', ['CMSTabsList'])
 
-        # Adding model 'SingleTab'
-        db.create_table('cmsplugin_tabs_singletab', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('plugin', self.gf('django.db.models.fields.related.ForeignKey')(related_name='tabs', to=orm['cmsplugin_tabs.CMSTabsList'])),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=32)),
-            ('content', self.gf('django.db.models.fields.TextField')()),
-            ('order', self.gf('django.db.models.fields.PositiveIntegerField')(default=1, db_index=True)),
+        # Adding model 'CMSSingleTab'
+        db.create_table(u'cmsplugin_tabs_cmssingletab', (
+            (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(default='', max_length=32, blank=True)),
+            ('is_strong', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('content', self.gf('djangocms_text_ckeditor.fields.HTMLField')(default='', blank=True)),
         ))
-        db.send_create_signal('cmsplugin_tabs', ['SingleTab'])
+        db.send_create_signal(u'cmsplugin_tabs', ['CMSSingleTab'])
 
 
     def backwards(self, orm):
         # Deleting model 'CMSTabsList'
-        db.delete_table('cmsplugin_cmstabslist')
+        db.delete_table(u'cmsplugin_tabs_cmstabslist')
 
-        # Deleting model 'SingleTab'
-        db.delete_table('cmsplugin_tabs_singletab')
+        # Deleting model 'CMSSingleTab'
+        db.delete_table(u'cmsplugin_tabs_cmssingletab')
 
 
     models = {
         'cms.cmsplugin': {
             'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
             'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
@@ -51,20 +53,21 @@ class Migration(SchemaMigration):
         'cms.placeholder': {
             'Meta': {'object_name': 'Placeholder'},
             'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
         },
-        'cmsplugin_tabs.cmstabslist': {
-            'Meta': {'object_name': 'CMSTabsList', 'db_table': "'cmsplugin_cmstabslist'", '_ormbases': ['cms.CMSPlugin']},
-            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'})
+        u'cmsplugin_tabs.cmssingletab': {
+            'Meta': {'object_name': 'CMSSingleTab', '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'content': ('djangocms_text_ckeditor.fields.HTMLField', [], {'default': "''", 'blank': 'True'}),
+            'is_strong': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'default': "''", 'max_length': '32', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         },
-        'cmsplugin_tabs.singletab': {
-            'Meta': {'ordering': "['order']", 'object_name': 'SingleTab'},
-            'content': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1', 'db_index': 'True'}),
-            'plugin': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tabs'", 'to': "orm['cmsplugin_tabs.CMSTabsList']"}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        u'cmsplugin_tabs.cmstabslist': {
+            'Meta': {'object_name': 'CMSTabsList', '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'template': ('django.db.models.fields.CharField', [], {'default': "'cmsplugin_tabs/tabs.html'", 'max_length': '255'})
         }
     }
 
